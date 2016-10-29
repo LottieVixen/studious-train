6bf80054b4b6f0c1a1754214e6d952ec8add51b2
Charlotte Lily Fields <foxsan48@gmail.com>
2016-10-29 19:31:50 +1000

    another nonworking attempt

--- http-uppercaser.js
+++ http-uppercaser.js
@@ -3,12 +3,12 @@ Take POST request from port specified as first argv,
 change to uppercase,
 send to response.
 */
-var stream = require('stream')
 var http = require('http');
-var upper = new stream.Transform();
+/*
+var stream = require('stream')
+var upper = new stream.Transform({decodeStrings: false});
 
 upper._transform = function (chunk, encoding, done){
-	console.error(chunk.toUpperCase());
 	done(null,chunk.toUpperCase());
 };
 
@@ -17,9 +17,33 @@ server = http.createServer(function(request, response){
 		requset.end();
 	})
 	request.on('POST',function(data){
+		console.error(data);
 		data.pipe(upper).pipe(response)
 		requset.end();
 	});
 });
 
 server.listen(process.argv[2]);
+/*/
+const Transform = require('stream').Transform;
+
+const upper = new Transform();
+upper._transform = function (chunk, encoding, callback){
+	callback(null,data.toUpperCase());
+}
+
+server = http.createServer(function(request, response){
+	request.on('error',function(data){
+		requset.end();
+	})
+	request.on('POST',function(data){
+		console.error(data);
+		data.pipe(upper).pipe(response)
+		requset.end();
+	});
+});
+
+server.listen(process.argv[2]);
+
+/*/
+//*/
